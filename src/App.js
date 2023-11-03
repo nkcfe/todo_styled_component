f1612ef33958ba3babd591fec80010f82ac8d97b
nkc930604@gmail.com <nkc930604@gmail.com>
2023-11-03 19:12:03 +0900

    final

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { createGlobalStyle } from "styled-components";
 import TodoTemplates from "./components/TodoTemplates";
 import TodoHead from "./components/TodoHead";
 import TodoList from "./components/TodoList";
+import TodoCreate from "./components/TodoCreate";
+import { TodoProvider } from "./TodoContext";
 
 // styled-components에서 글로벌 스타일을 추가하고 싶을 때 사용.
 const GlobalStyle = createGlobalStyle`
@@ -13,13 +15,14 @@ const GlobalStyle = createGlobalStyle`
 
 function App() {
   return (
-    <>
+    <TodoProvider>
       <GlobalStyle />
       <TodoTemplates>
         <TodoHead />
         <TodoList />
+        <TodoCreate />
       </TodoTemplates>
-    </>
+    </TodoProvider>
   );
 }
 
